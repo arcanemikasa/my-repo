089987e0ec3911ef10dbf7dce68b7d37165bd887
Mikasa <138040916+arcanemikasa@users.noreply.github.com>
2023-07-02 02:22:57 +0100

    Create func.py

---
+++ func.py
@@ -0,0 +1,14 @@
+def addTwo(x):
+	return x+2
+newNum = addTwo(8)
+print(newNum)
+
+def power(x,y):
+	return x**y
+power = power(5,3)
+print(power)
+
+def greet(name):
+	print("Hello",name)
+greet("Daniel")
+greet("Max")
